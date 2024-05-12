d64983346bcc557cb52c7c53a4a06123af27ba23
Sofuncia <sofia.nakonechna@gmail.com>
2024-05-13 01:11:42 +0300

    changes in reader.py

--- reader.py
+++ reader.py
@@ -5,16 +5,23 @@ PIL.Image.ANTIALIAS = PIL.Image.LANCZOS
 
 
 
+def split_lines(text):
+    split_text = """"""
+    for line in text.split("\n"):
+        split_text += line + """
+"""
+    return split_text
+
+
 def image_reader(image):
     reader = ocr.Reader(['uk'], gpu=False)
     result = reader.readtext(image)
-    if len(result) > 1:
-        full_text = ""
-        for i in range(len(result)):
-            print(full_text)
-            full_text += result[i][1] + "\n"
-        return full_text[0]
-    else: return result[0][1]
+    merged_text = ""
+    full_text = """"""
+    for i in range(len(result)):
+        merged_text += result[i][1] + "\n"
+        full_text = split_lines(merged_text)
+    return full_text
 
 
 

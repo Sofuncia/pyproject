68bc77f2761f5f8565de2148663cf67d10201667
Sofuncia <sofia.nakonechna@gmail.com>
2024-05-13 00:30:59 +0300

    edited reader.py

--- reader.py
+++ reader.py
@@ -3,11 +3,15 @@ import easyocr as ocr
 import PIL
 PIL.Image.ANTIALIAS = PIL.Image.LANCZOS
 
+
+
 def image_reader(image):
     reader = ocr.Reader(['uk'], gpu=False)
     result = reader.readtext(image)
     if len(result) > 1:
-        for i in result:
+        full_text = ""
+        for i in range(len(result)):
+            print(full_text)
             full_text += result[i][1] + "\n"
         return full_text[0]
     else: return result[0][1]

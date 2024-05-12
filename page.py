ef598eaca48e8cfaeae4ada97760dc48d23be762
Sofuncia <sofia.nakonechna@gmail.com>
2024-05-12 22:53:36 +0300

    second commit

--- page.py
+++ page.py
@@ -1,12 +1,16 @@
 import streamlit as st
 from reader import image_reader
 
-st.title("First hello")
-st.write("""### Hello hello!""")
+st.title("Онлайн зчитувач тексту із зображення")
+st.write("""### Підтримувані типи файлів:""")
+st.markdown("- JPG")
+st.markdown("- PNG")
+st.markdown("- TIFF")
 
-uploaded_file = st.file_uploader("Choose a file")
-# To read file as bytes:
-bytes_data = uploaded_file.getvalue()
+
+uploaded_file = st.file_uploader("Оберіть файл зображення")
+bytes_data = uploaded_file.read()
+
 if uploaded_file:
     recognized_text = image_reader(bytes_data)
     st.code(recognized_text, language="python")

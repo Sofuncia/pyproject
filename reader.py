ba6a1d2f57c87dcd51b533cb763359bb7474372c
Sofuncia <sofia.nakonechna@gmail.com>
2024-05-12 21:41:27 +0300

    change in reader.py

--- reader.py
+++ reader.py
@@ -2,11 +2,16 @@ import torch
 import easyocr as ocr
 import PIL
 PIL.Image.ANTIALIAS = PIL.Image.LANCZOS
+import streamlit as st
+from io import StringIO
 
 def image_reader(image):
+    # To read file as bytes:
+    bytes_data = image.getvalue()
     reader = ocr.Reader(['uk'], gpu=False)
-    result = reader.readtext(image)
+    result = reader.readtext(bytes_data)
     return result[0][1]
+
 
 
 

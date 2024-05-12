a429e8e8b221478420f912d3a98b26f272433f61
Sofuncia <sofia.nakonechna@gmail.com>
2024-05-12 21:06:04 +0300

    changes in page.py

--- page.py
+++ page.py
@@ -5,5 +5,7 @@ st.title("First hello")
 st.write("""### Hello hello!""")
 
 uploaded_file = st.file_uploader("Choose a file")
+# To read file as bytes:
+bytes_data = uploaded_file.getvalue()
 if uploaded_file:
-    st.write(image_reader(uploaded_file))
+    st.write(image_reader(bytes_data))

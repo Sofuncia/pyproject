8ad15f54a12fa8099b8e0ca5fdcc8991acc0c99f
Sofuncia <sofia.nakonechna@gmail.com>
2024-05-15 00:59:52 +0300

    edit page.py

--- page.py
+++ page.py
@@ -9,9 +9,9 @@ page.show_details()
 
 upload_file = st.file_uploader("Оберіть файл зображення")
 
-if (upload_file is not None):
+if upload_file is not None:
     if is_file_type_supported(upload_file) is True:
-        file_bytes = upload_file.read()
-        uploaded_file = Image(file_bytes)
+        uploaded_file = Image(upload_file.read())
+        upload_file.read_image()
     else: st.error("Тип обраного файлу не підтримується")
 else: pass
